0c8587eb103058f4e7a1d6dd7c289c9238a95fd7
Harsha Vykunta <harshavardhan.biit@gmail.com>
2019-02-21 14:37:16 -0800

    added hashmaps for student, course and lectures for inMemoryDatabase

--- src/main/java/com/harsha/cloudcomputing/courseservice/datamodel/InMemoryDatabase.java
+++ src/main/java/com/harsha/cloudcomputing/courseservice/datamodel/InMemoryDatabase.java
@@ -7,8 +7,36 @@ import java.util.HashMap;
  */
 public class InMemoryDatabase {
     private static HashMap<Long, Professor> professorDB = new HashMap<>();
+    private static HashMap<Long, Student> studentDB = new HashMap<>();
+    private static HashMap<Long, Course> courseDB = new HashMap<>();
+    private static HashMap<Long, Lecture> lectureDB = new HashMap<>();
 
+    /**
+     * @return the professorDB
+     */
     public static HashMap<Long, Professor> getProfessorDB() {
         return professorDB;
     }
+
+    /**
+     * @return the lectureDB
+     */
+    public static HashMap<Long, Lecture> getLectureDB() {
+        return lectureDB;
+    }
+
+    /**
+     * @return the studentDB
+     */
+    public static HashMap<Long, Student> getStudentDB() {
+        return studentDB;
+    }
+
+    /**
+     * @return the courseDB
+     */
+    public static HashMap<Long, Course> getCourseDB() {
+        return courseDB;
+    }
+
 }
