e2fa82e68e7ddeb731ea3a816fb604814fda7745
Harsha Vykunta <harshavardhan.biit@gmail.com>
2019-02-21 23:51:25 -0800

    updating wording in comment

--- src/main/java/com/harsha/cloudcomputing/courseservice/resources/TestResource.java
+++ src/main/java/com/harsha/cloudcomputing/courseservice/resources/TestResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
- * Root resource (exposed at "myresource" path)
+ * TestResource (exposed at "test" path)
  */
 @Path("/test")
 public class TestResource {
