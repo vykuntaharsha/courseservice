44f4fbf31567eff2a66f3a6ad10ace68cc00e12c
Harsha Vykunta <harshavardhan.biit@gmail.com>
2019-03-02 15:58:24 -0800

    adding logger

--- src/main/java/com/harsha/cloudcomputing/courseservice/resources/ServerHealthResource.java
+++ src/main/java/com/harsha/cloudcomputing/courseservice/resources/ServerHealthResource.java
@@ -3,11 +3,14 @@ package com.harsha.cloudcomputing.courseservice.resources;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import com.jcabi.log.Logger;
 
 import io.swagger.annotations.Api;
 
@@ -16,7 +19,7 @@ import io.swagger.annotations.Api;
  */
 @Api
 @Path("/health")
-public class TestResource {
+public class ServerHealthResource {
 
     /**
      * Method handling HTTP GET requests. The returned object will be sent to the
@@ -26,11 +29,14 @@ public class TestResource {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+
     public Map<String, String> getHealth() {
 
         Map<String, String> resultObj = new HashMap<>();
         resultObj.put("status", "good");
         resultObj.put("serverTime", new Date().toString());
+        Logger.info(this, resultObj.toString());
+
         return resultObj;
     }
 }
